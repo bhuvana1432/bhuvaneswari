c0ef38b15a1c91f48b85bd40a894def5857d60ba
Bhuva <Bhuva@DESKTOP-3LGESEG>
2022-01-29 18:57:31 +0530

    first commit

--- Myproject/src/com/eb/entity/Domestic.java
+++ Myproject/src/com/eb/entity/Domestic.java
@@ -1,11 +1,22 @@
 package com.eb.entity;
 
 public class Domestic extends Connection {
-
-	@Override
+	public Domestic(int currentReading, int previousReading, float[] slabs) {
+		super(currentReading, previousReading, slabs);
 	public float computeBill() {
 		// TODO Auto-generated method stub
-		return 0;
+		[4:18 PM, 1/29/2022] Lucky ❤️❤️: int units=currentReading=previousReading;
+		float bill=0;
+		if(units<=50) {
+			bill=units*slabs[0];
+		}
+		else if(units<=100) {
+			bill=115+(units=50)*slabs[1];
+		}
+		else if(units>100) {
+			bill=325+(units-100)*slabs[2];
+		}
+		return bill;
 	}
 
-}
+
